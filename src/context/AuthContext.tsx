8fe4c9eac7a72e8e051dd7500afd71a2365c6053
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2023-12-10 20:30:14 -0300

    Fazendo formulario de edição funcionar

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,13 +6,17 @@ export interface User {
     name: string,
     token: string,
     img: string,
-    email: string
+    email: string,
+    id: number,
+    funcao: string,
+    telefone: string,
 }
 
 interface AuthContextProps {
     user: User
     login: (nome: string, senha: string) => void
     logout: () => void
+    updateUser: (newUser: User) => void
     loading: boolean
     validToken: boolean
     verifyToken: (token: string) => Promise<boolean>
@@ -33,9 +37,9 @@ export const AuthComponent = ({children}: AuthComponentProps) => {
     const login = async (email: string, senha: string) => {
         try {
             const { data } = await api.post("/responsavel/login", {email, senha});
-            if (data.token) {
-                setUser({ name: data.nome, token: data.token, img: data.img, email: data.email });
-                saveUser({ name: data.nome, token: data.token, img: data.img, email: data.email });
+            if (data.token) {                
+                setUser({ name: data.nome, token: data.token, img: data.img, email: data.email, funcao: data.funcao, id: data.id, telefone: data.telefone });
+                saveUser({ name: data.nome, token: data.token, img: data.img, email: data.email, funcao: data.funcao, id: data.id, telefone: data.telefone });
                 setValidToken(true);
             }
         } catch (error) {
@@ -43,10 +47,16 @@ export const AuthComponent = ({children}: AuthComponentProps) => {
         }
     }
 
+    const updateUser = async (newUser: any) => {
+        setUser({ name: newUser.nome, token: user.token, img: newUser.imagem, email: newUser.email, funcao: newUser.funcao, id: newUser.id, telefone: newUser.telefone });
+        await saveUser({ name: newUser.nome, token: user.token, img: newUser.imagem, email: newUser.email, funcao: newUser.funcao, id: newUser.id, telefone: newUser.telefone });
+        getUser();
+    }
+
     const logout = () => {
         try {
-            setUser({ name: "", token: "", img: "", email: ""});
-            saveUser({ name: "", token: "", img: "", email: ""});
+            setUser({ name: "", token: "", img: "", email: "", funcao: "", id: 0, telefone: ""});
+            saveUser({ name: "", token: "", img: "", email: "", funcao: "", id: 0, telefone: ""});
             setValidToken(false);
             } catch (error) {
 
@@ -61,8 +71,8 @@ export const AuthComponent = ({children}: AuthComponentProps) => {
             return setUser(user);
         }
         setLoading(false);
-        setUser({ name: "", token: "", img: "", email: "" });
-        saveUser({ name: "", token: "", img: "", email: "" });
+        setUser({ name: "", token: "", img: "", email: "", funcao: "", id: 0, telefone: "" });
+        saveUser({ name: "", token: "", img: "", email: "", funcao: "", id: 0, telefone: "" });
     }
 
     const verifyToken = async (token: string) => {
@@ -84,7 +94,7 @@ export const AuthComponent = ({children}: AuthComponentProps) => {
 
 
     return (
-    <AuthContext.Provider value={{ user, login, logout, validToken, verifyToken, loading }}>
+    <AuthContext.Provider value={{ user, login, logout, validToken, verifyToken, loading, updateUser }}>
         {children}
     </AuthContext.Provider>
     )
