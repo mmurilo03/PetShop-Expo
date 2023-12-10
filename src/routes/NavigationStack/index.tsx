eb5b38f37b1b9b1753263149c1972b29b5876a37
mmurilo03 <105663942+mmurilo03@users.noreply.github.com>
2023-12-10 13:46:16 -0300

    Criando tela de pets

--- src/routes/NavigationStack/index.tsx
+++ src/routes/NavigationStack/index.tsx
@@ -4,16 +4,21 @@ import { Home } from "../../screens/Home";
 import { Login } from "../../screens/Login";
 import { Starting } from '../../screens/Starting';
 import { Responsaveis } from '../../screens/Responsaveis';
+import { Pets } from '../../screens/Pets';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
 export function NavigationStack(){
 
   return (
-    <Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-      <Screen name="Home" component={Home} />
+    <Navigator initialRouteName="Home" screenOptions={{
+      headerShown: false,
+      animation: 'none',
+      }}>
+      <Screen name="Home" component={Home} options={{
+      }}/>
       <Screen name="Responsaveis" component={Responsaveis} />
-      <Screen name="Pets" component={Home} />
+      <Screen name="Pets" component={Pets} />
     </Navigator>
   )
 }
